961338b9fa1547ec361fd485f69351eefe519723
sethblumer1 <sethblumer1@gmail.com>
2024-02-29 18:58:47 -0500

    with indeed working

--- background.ts
+++ background.ts
@@ -52,3 +52,74 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
     handleMessage(msg, response);
     return true;
 })
+
+// // decompress array buffer, turn to string, then parse as json
+// function arrayBufferToJson(arrayBuffer) {
+//     const jsonString = Pako.inflate(new Uint8Array(arrayBuffer), { to: 'string' });
+//     try {
+//         const json = JSON.parse(jsonString);
+//         return json;
+//     } catch (error) {
+//         console.error("Parsing error:", error);
+//         return null;
+//     }
+// }
+
+// // To turn array buffer into json, because details.requestBody returns array buffer
+// function arrayBufferToJson(arrayBuffer) {
+//     // Step 1: Convert ArrayBuffer to String
+//     const decoder = new TextDecoder("utf-8");
+//     const jsonString = decoder.decode(arrayBuffer);
+//     console.log("JSON String:", jsonString);
+
+//     // A lot of the times the JSON is compressed? so we need to decompress it
+
+//     // Step 2: Parse the String as JSON
+//     try {
+//         const json = JSON.parse(jsonString);
+//         return json;
+//     } catch (error) {
+//         console.error("Parsing error:", error);
+//         return null; // or handle the error as you see fit
+//     }
+// }
+
+
+// // Network request logging
+// chrome.webRequest.onBeforeRequest.addListener(
+//     (details) => {
+//         // console.log(details)
+//         // if (details.requestBody && details.requestBody.raw && details.requestBody.raw.length > 0) {
+//         //     const arrayBuffer = details.requestBody.raw[0].bytes;
+//         //     if (arrayBuffer) {
+//         //         const json = arrayBufferToJson(arrayBuffer);
+//         //         console.log("Request JSON:", json);
+//         //     }
+//         // }
+
+//         if (details.url.includes("https://www.linkedin.com/voyager/api/voyagerFeedDashSaveStates")) {
+//             const reqBody = details.requestBody;
+//             if (reqBody && reqBody.raw && reqBody.raw.length > 0) {
+//                 const reqBodyArrayBuffer = reqBody.raw[0].bytes;
+//                 // console.log(details.)
+//                 // const reqBodyJson = arrayBufferToJson(reqBodyArrayBuffer);
+
+//                 // if (reqBodyArrayBuffer) {
+//                 //     const reqBodyJson = arrayBufferToJson(reqBodyArrayBuffer);
+//                 //     console.log("Request JSON:", reqBodyJson);
+//                 // }
+//             }
+
+
+//             // if (reqBody && reqBody.raw && reqBody.raw.length > 0) {
+//             //     const arrayBuffer = reqBody.raw[0].bytes;
+//             //     if (arrayBuffer) {
+//             //         const json = arrayBufferToJson(arrayBuffer);
+//             //         console.log("Request JSON:", json);
+//             //     }
+//             // }
+//         }
+//     },
+//     { urls: ["<all_urls>"] },
+//     ["requestBody", "requestHeaders"]
+// );
--- public/assets/js/initializeUI.js
+++ public/assets/js/initializeUI.js
@@ -2,15 +2,15 @@
 // Solution is to create site configurations so it is flexible; then check host name
 
 const siteConfigs = {
-  'www.linkedin.com': {
-    jobTitleSelector: '.job-details-jobs-unified-top-card__job-title-link',
+  "www.linkedin.com": {
+    jobTitleSelector: ".job-details-jobs-unified-top-card__job-title-link",
     companyNameSelector:
-      '.job-details-jobs-unified-top-card__primary-description-without-tagline.mb2 a:first-of-type',
-    removeUrlPart: '/life',
+      ".job-details-jobs-unified-top-card__primary-description-without-tagline.mb2 a:first-of-type",
+    removeUrlPart: "/life",
   },
-  'www.indeed.com': {
-    jobTitleSelector: '.jobsearch-JobInfoHeader-title',
-    companyNameSelector: '#companyLink',
+  "www.indeed.com": {
+    jobTitleSelector: ".jobsearch-JobInfoHeader-title",
+    companyNameSelector: "#companyLink",
   },
 };
 
@@ -18,71 +18,59 @@ const currentConfig = siteConfigs[window.location.hostname];
 
 console.log(currentConfig);
 
-let lastJobTitle = '';
+let lastJobTitle = "";
 
 function removeLifeFromUrl(url) {
-  if (url.endsWith('/life')) {
+  if (url.endsWith("/life")) {
     return url.slice(0, -5);
   }
   return url;
 }
 
-// TEST WITH CONFIG
-function handleJobData() {
-  if (!currentConfig) return;
-
-  let currJobTitle = document.querySelector(currentConfig.jobTitleSelector);
-  let companyName = document.querySelector(currentConfig.companyNameSelector);
-
-  if (currJobTitle && companyName) {
-    currJobTitle = currJobTitle.textContent.trim();
-    companyName = companyName.textContent.trim();
-    let companyUrl;
-
-    // Use currentConfig.removeUrlPart if needed, and check if it exists
-    if (currentConfig === 'www.linkedin.com') {
-      companyUrl = removeLifeFromUrl(companyName.getAttribute('href'));
-    } else {
-      companyUrl = 'www.google.com';
-    }
-
-    if (currJobTitle !== lastJobTitle) {
-      lastJobTitle = currJobTitle; // Update the last seen job posting
-      const jobData = {
-        jobTitle: currJobTitle,
-        companyName,
-        companyUrl,
-      };
-
-      // Add event listener to new save button
-      // const saveButton = document.querySelector('.jobs-save-button');
-
-      // if (saveButton) {
-      //   saveButton.addEventListener('click', () => {
-      //     console.log('Hi');
-      //   });
-      // } else {
-      //   console.log('Jobs save button not found');
-      // }
-
-      window.postMessage({ type: 'FROM_EXTENSION', data: jobData }, '*');
-    }
+function attachEventListenerToSaveButton() {
+  const saveButton = document.querySelector(".jobs-save-button");
+  if (saveButton && !saveButton.dataset.listenerAttached) {
+    saveButton.addEventListener("click", () => {
+      console.log("Save button clicked");
+      // Handle the click event
+    });
+    saveButton.dataset.listenerAttached = "true";
+    console.log("Event listener attached to save button");
   }
 }
 
-// PREVIOUS THAT WORKED WITH LINKEDIN
+// TEST WITH CONFIG
 // function handleJobData() {
-//   let currJobTitle = document.querySelector(
-//     '.job-details-jobs-unified-top-card__job-title-link'
-//   );
-//   let companyName = document.querySelector(
-//     '.job-details-jobs-unified-top-card__primary-description-without-tagline.mb2 a:first-of-type'
-//   );
+//   if (!currentConfig) return;
 
-//   if (currJobTitle && companyName) {
-//     currJobTitle = currJobTitle.textContent.trim();
-//     const companyUrl = removeLifeFromUrl(companyName.getAttribute('href'));
-//     companyName = companyName.textContent.trim();
+//   let currJobTitle = document.querySelector(currentConfig.jobTitleSelector);
+//   let companyName = document.querySelector(currentConfig.companyNameSelector);
+
+//   console.log(currJobTitle);
+//   // console.log(companyName);
+
+//   if (currJobTitle) {
+//     if (window.location.hostname === "www.indeed.com") {
+//       currJobTitle = currJobTitle.children[0].textContent;
+//       // console.log(currJobTitle.children);
+//     } else {
+//       currJobTitle = currJobTitle.textContent.trim();
+//     }
+
+//     if (window.location.hostname === "www.indeed.com") {
+//       companyName = "Indeed sucks";
+//     } else {
+//       companyName = companyName.textContent.trim();
+//     }
+
+//     let companyUrl;
+
+//     // Use currentConfig.removeUrlPart if needed, and check if it exists
+//     if (currentConfig === "www.linkedin.com") {
+//       companyUrl = removeLifeFromUrl(companyName.getAttribute("href"));
+//     } else {
+//       companyUrl = "www.google.com";
+//     }
 
 //     if (currJobTitle !== lastJobTitle) {
 //       lastJobTitle = currJobTitle; // Update the last seen job posting
@@ -92,46 +80,151 @@ function handleJobData() {
 //         companyUrl,
 //       };
 
-//       // Add event listener to new save button
-//       // const saveButton = document.querySelector('.jobs-save-button');
-
-//       // if (saveButton) {
-//       //   saveButton.addEventListener('click', () => {
-//       //     console.log('Hi');
-//       //   });
-//       // } else {
-//       //   console.log('Jobs save button not found');
-//       // }
-
-//       window.postMessage({ type: 'FROM_EXTENSION', data: jobData }, '*');
+//       window.postMessage({ type: "FROM_EXTENSION", data: jobData }, "*");
 //     }
 //   }
 // }
 
-// Set up a mutation observer to watch for changes in the job details container
-const observer = new MutationObserver((mutations) => {
-  for (let mutation of mutations) {
-    if (mutation.type === 'childList' || mutation.type === 'subtree') {
-      handleJobData(); // Check for and handle job data if the DOM has changed
-    }
+function handleJobData(retryCount = 0) {
+  if (!currentConfig) return;
+
+  const currJobTitleElement = document.querySelector(
+    currentConfig.jobTitleSelector
+  );
+  const companyNameElement = document.querySelector(
+    currentConfig.companyNameSelector
+  );
+
+  if (!currJobTitleElement && retryCount < 3) {
+    // If job title element is not found, retry up to 3 times.
+    console.log(
+      `Job title element not found. Retrying... Attempt ${retryCount + 1}`
+    );
+    setTimeout(() => handleJobData(retryCount + 1), 500); // Wait 1 second before retrying.
+    return;
   }
-});
 
-// Start observing the target node for configured mutations
-const targetNode = document; // Replace with a more specific container if possible
-observer.observe(targetNode, { childList: true, subtree: true });
+  // If the element is found or retries have been exhausted, proceed to process the data.
+  let currJobTitle = currJobTitleElement
+    ? currJobTitleElement.textContent.trim()
+    : "";
+  let companyName = companyNameElement
+    ? companyNameElement.textContent.trim()
+    : "Company Name Not Found";
 
-// Initial check
-handleJobData();
+  // Special handling for Indeed to ensure we have the correct data format.
+  if (window.location.hostname === "www.indeed.com") {
+    currJobTitle =
+      currJobTitleElement && currJobTitleElement.children.length > 0
+        ? currJobTitleElement.children[0].textContent.trim()
+        : "Job Title Not Found";
+    companyName = "Indeed Company"; // Example, adjust according to your needs.
+  }
+
+  // Further processing (e.g., forming the company URL) remains unchanged.
+  let companyUrl =
+    currentConfig === "www.linkedin.com"
+      ? removeLifeFromUrl(companyNameElement.getAttribute("href"))
+      : "www.google.com";
+
+  // Check if we have a new job title compared to the last one processed.
+  if (currJobTitle && currJobTitle !== lastJobTitle) {
+    lastJobTitle = currJobTitle; // Update the last seen job posting.
+    const jobData = {
+      jobTitle: currJobTitle,
+      companyName,
+      companyUrl,
+    };
+
+    // Perform your action with the job data, e.g., posting a message.
+    window.postMessage({ type: "FROM_EXTENSION", data: jobData }, "*");
+    console.log("Job data processed:", jobData);
+  } else if (!currJobTitle) {
+    console.log("Failed to fetch job title after retries.");
+  }
+}
+
+function waitForElementAndObserve(selector, observeCallback) {
+  const intervalId = setInterval(() => {
+    const targetNode = document.querySelector(selector);
+    console.log(`Checking for ${selector}`);
+    if (targetNode) {
+      console.log(`Found ${selector}, setting up observer.`);
+      clearInterval(intervalId); // Stop the interval
+      observeCallback(targetNode); // Call the callback function with the found node
+    }
+  }, 1000); // Check every 1000 milliseconds (1 second)
+}
+
+function setupObserver(targetNode) {
+  const observer = new MutationObserver((mutations) => {
+    // console.log(mutations.length);
+    for (let mutation of mutations) {
+      if (mutation.type === "childList" || mutation.type === "subtree") {
+        if (window.location.hostname === "www.indeed.com") {
+          // console.log(mutation);
+          if (mutation.target.id === "js-match-insights-provider-job-details") {
+            // console.log("hi");
+
+            handleJobData();
+            attachEventListenerToSaveButton();
+
+            // handleJobData();
+            // attachEventListenerToSaveButton();
+          }
+        } else {
+          handleJobData(); // Check for and handle job data if the DOM has changed
+          attachEventListenerToSaveButton();
+        }
+      }
+    }
+  });
+
+  observer.observe(targetNode, { childList: true, subtree: true });
+  // Perform an initial check in case the content is already present
+  handleJobData();
+  attachEventListenerToSaveButton();
+}
+
+// Use the modified function to wait for the element before observing
+if (window.location.hostname === "www.linkedin.com") {
+  waitForElementAndObserve(
+    ".jobs-search__job-details--container",
+    setupObserver
+  );
+}
+
+if (window.location.hostname === "www.indeed.com") {
+  waitForElementAndObserve(".jobsearch-RightPane", setupObserver);
+}
+
+// // Set up a mutation observer to watch for changes in the job details container
+// const observer = new MutationObserver((mutations) => {
+//   for (let mutation of mutations) {
+//     if (mutation.type === "childList" || mutation.type === "subtree") {
+//       handleJobData(); // Check for and handle job data if the DOM has changed
+//     }
+//   }
+// });
+
+// // Start observing the target node for configured mutations
+// const targetNode = document.querySelector(
+//   ".jobs-search__job-details--container"
+// ); // This is null initially, so we have to wait
+// console.log(targetNode);
+// observer.observe(targetNode, { childList: true, subtree: true });
+
+// // Initial check
+// handleJobData();
 
 // See if save button clicked
 
 (async () => {
-  const app = document.createElement('div');
+  const app = document.createElement("div");
 
-  app.id = 'root';
+  app.id = "root";
   document.body.append(app);
 
-  const src = chrome?.runtime?.getURL('/react/index.js');
+  const src = chrome?.runtime?.getURL("/react/index.js");
   await import(src);
 })();
